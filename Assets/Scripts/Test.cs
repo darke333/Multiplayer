fc4a92043b0057809d37029a1debd1cc3b7402fb
SirotenkoMax <m.sirotenko@only1p.com>
2023-09-02 20:50:18 +0300

    Character added

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class Test1
+{
+    private readonly Test2 _test2;
+    
+}
+
+public class Test2
+{
+    
+}
